85f544d7f6534432d6fd4f1772c0a2ae270abf25
Tanwarat066 <68030121@kmitl.ac.th>
2025-06-19 07:24:24 +0000

    er

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,1 +1,12 @@
-print("RTFM (Read the F-ing manual)")
+import sys
+def main():
+    args = sys.argv[1:]
+    if len(args) < 2:
+       print ("none")
+       return
+
+    for arg in reversed(args) :
+        print(arg)
+
+if __name__ == "__main__":
+   main()
